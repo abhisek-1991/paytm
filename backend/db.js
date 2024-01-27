609643caf22f81b8a5e88b1284634f54edd95a0c
user <abhisek.may1991@gmail.com>
2024-01-28 05:12:36 +0530

    backend half completed

---
+++ backend/db.js
@@ -0,0 +1,52 @@
+const moongose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL);
+const userSchema = moongose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+
+const Account = moongose.model("Account", accountSchema);
+const User = moongose.model("User", userSchema);
+
+module.exports = {
+    Account,
+    User
+}
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express");
+require('dotenv').config();
 
+const cors = require('cors');
+const rootRouter = require('./routes/index');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1', rootRouter);
 
+app.listen(3000);
