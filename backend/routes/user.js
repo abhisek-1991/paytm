963768ea8f163564e35dd93c5df2c1b1afc38623
user <abhisek.may1991@gmail.com>
2024-01-28 06:32:02 +0530

    dummy balance on signup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,42 +2,54 @@ const express = require('express');
 const zod = require('zod');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = "yoyo";
-const User = require('../db');
+const { JWT_SECRET } = require('../config')
+const {User,Account} = require('../db');
 const { authMiddleware } = require('../middleware');
 
-const signupSchema = zod.object({
+const signupBody = zod.object({
     username: zod.string().email(),
-    password: zod.string(),
-    firstName: zod.string(),
-    lastName: zod.string(),
+	firstName: zod.string(),
+	lastName: zod.string(),
+	password: zod.string()
 })
-router.post('/signup', async(req, res) => {
-    const body = req.body;
-    const { success } = signupSchema.safeParse(req.body);
+
+router.post("/signup", async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
     if (!success) {
-        return res.json({
+        return res.status(411).json({
             message: "Email already taken / Incorrect inputs"
         })
     }
 
-    const user = User.findOne({
-        username: body.username
+    const existingUser = await User.findOne({
+        username: req.body.username
     })
 
-    if (user._id) {
-        return res.json({
+    if (existingUser) {
+        return res.status(411).json({
             message: "Email already taken/Incorrect inputs"
         })
     }
 
-    const dbUser = await User.create(body);
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+    const userId = user._id;
+    await Account.create({
+        userId,
+        balance: 1 + Math.random() * 10000
+    })
+
     const token = jwt.sign({
-        userId: dbUser._id
+        userId
     }, JWT_SECRET);
+
     res.json({
         message: "User created successfully",
-        token:token
+        token: token
     })
 })
 
